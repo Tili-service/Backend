2a488ba18e9566266ec67e736d5533f56ce6dab6
Marin <marin.lamy@epitech.eu>
2026-02-25 09:10:17 +0100

    fix(catalogue): add missing comma in catalogue table definition

--- scheme.sql
+++ scheme.sql
@@ -50,7 +50,7 @@ CREATE TABLE catalogue (
     catalogue_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2),
-    tva SMALLINT
+    tva SMALLINT,
     categorie_id INTEGER REFERENCES categorie(categorie_id),
     image_id INTEGER REFERENCES image(image_id)
 );

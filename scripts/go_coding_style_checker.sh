ba1fce4e9657c9b9815f014e48cadfa27f57691a
Matisse-M <matisse.marsac@epitech.eu>
2026-03-02 16:54:18 +0100

    feat(cicd): implement basis of golang linter check

---
+++ scripts/go_coding_style_checker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if gofmt is installed in ./backend
+if ! command -v gofmt &> /dev/null
+then
+    echo -e "\033[1;31mgofmt could not be found. Please install Go to use this script.\033[0m"
+    echo -e "You can install it by running: \033[1;33mgo install golang.org/x/tools/cmd/gofmt@latest\033[0m"
+    exit 1
+fi
+
+# Check if gofmt is properly formatted
+if ! gofmt -l $(PWD)/app | grep -q .
+then
+    echo -e "\033[1;29mgofmt: All files are properly formatted.\033[0m"
+else
+    echo -e "\033[1;29mgofmt: The following files are not properly formatted:\033[0m"
+    gofmt -l $(PWD)/app > $(PWD)/scripts/gofmt_output.txt
+    for file in $(cat $(PWD)/scripts/gofmt_output.txt); do
+        echo -e "\033[0;31m$file\033[0m"
+    done
+    exit 1
+fi
+
+echo -e "\033[1;32mAll checks passed successfully!\033[0m"
+exit 0

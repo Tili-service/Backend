2043da6b690b8bd3173ce00158e374c5aa1a8316
Marin <marin.lamy@epitech.eu>
2026-03-09 10:43:08 +0100

    feat(token): add JWT token creation and validation functions

---
+++ app/internal/user/token.go
@@ -0,0 +1,57 @@
+package user
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret-key")
+
+func CreateToken(user User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"userID":      user.UserID,
+			"name":        user.Name,
+			"email":       user.Email,
+			"accessLevel": user.AccessLevel,
+			"exp":         time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (int64, error) {
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return 0, fmt.Errorf("invalid token: %w", err)
+	}
+
+	if !token.Valid {
+		return 0, fmt.Errorf("token is not valid")
+	}
+
+	claims, ok := token.Claims.(*jwt.MapClaims)
+	if !ok {
+		return 0, fmt.Errorf("invalid claims")
+	}
+
+	userID, ok := (*claims)["userID"].(float64)
+	if !ok {
+		return 0, fmt.Errorf("userID not found in token")
+	}
+
+	return int64(userID), nil
+}

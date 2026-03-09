e6a3805e4c6dd0d2ba65307cd6d3b9b755925c9c
Marin <marin.lamy@epitech.eu>
2026-03-09 11:14:21 +0100

    refactor(init-db): define SQL files in an array for better maintainability

--- database/init-db.sh
+++ database/init-db.sh
@@ -2,6 +2,17 @@
 
 set -e
 
-for f in /docker-entrypoint-initdb.d/tables/*.sql; do
-    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$f"
+files=(
+    "account.sql"
+    "categories.sql"
+    "image.sql"
+    "catalogue.sql"
+    "payment.sql"
+    "sales.sql"
+    "store.sql"
+    "user.sql"
+)
+
+for f in "${files[@]}"; do
+    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "/docker-entrypoint-initdb.d/tables/$f"
 done

cc43fa149a32f5dff86263c2205cdcc66c5ecbe6
Albane Merian <albane.merian@epitech.eu>
2026-03-11 10:10:55 +0100

    fix(catalogue): Update the catalogue table so that it only has the needed filed for a catalogue, and doesn't include items element

--- app/internal/catalogue/model.go
+++ app/internal/catalogue/model.go
@@ -1,21 +1,18 @@
 package catalogue
 
 import (
-	"tili/app/internal/categorie"
-	"tili/app/internal/image"
-
 	"github.com/uptrace/bun"
 )
 
 type Catalogue struct {
 	bun.BaseModel `bun:"table:catalogue,alias:c" swaggerignore:"true"`
 
-	CatalogueID int64                `bun:"catalogue_id,pk,autoincrement"                     json:"catalogue_id"`
+	CatalogueID int                  `bun:"catalogue_id,pk,autoincrement"                     json:"catalogue_id"`
 	Name        string               `bun:"name,notnull"                                      json:"name"`
-	Price       float64              `bun:"price,type:decimal(10,2)"                          json:"price"`
-	TVA         int16                `bun:"tva,type:smallint"                                 json:"tva"`
-	CategorieID int64                `bun:"categorie_id"                                      json:"categorie_id"`
-	ImageID     int64                `bun:"image_id"                                          json:"image_id"`
-	Categorie   *categorie.Categorie `bun:"rel:belongs-to,join:categorie_id=categorie_id"    json:"categorie,omitempty"`
-	Image       *image.Image         `bun:"rel:belongs-to,join:image_id=image_id"             json:"image,omitempty"`
+	Description string `bun:"description" json:"description"`
+}
+
+type CatalogueUpdate struct {
+	Name *string `json:"name"`
+	Description *string `json:"description"`
 }

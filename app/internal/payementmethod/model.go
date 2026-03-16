29350e2402a209e1a71034abe2d7af456521bece
Albane Merian <albane.merian@epitech.eu>
2026-03-16 10:02:06 +0100

    fix(model): Update the comment in the model.go fr the payement method

--- app/internal/payementmethod/model.go
+++ app/internal/payementmethod/model.go
@@ -5,8 +5,8 @@ import "github.com/uptrace/bun"
 type PayementMethod struct {
 	bun.BaseModel `bun:"table:payementmethod,alias:pm" swaggerignore:"true"`
 
-	PayementMethodID int    `bun:"payement_method_id,pk,autoincrement" json:"payement_method_id" example:"1"`
-	Name             string `bun:"name,notnull"                        json:"name"               example:"Credit Card"`
+	PayementMethodID int    `bun:"payement_method_id,pk,autoincrement" json:"payement_method_id" example:(1)`
+	Name             string `bun:"name,notnull"                        json:"name"               example:(Credit Card)`
 }
 
 // PayementMethodInput is the request body for creating or updating a payment method

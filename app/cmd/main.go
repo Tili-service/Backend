d1d0b0f4d6a4d8f1aad3f334d9bbc5b98b07d6e1
Albane Merian <albane.merian@epitech.eu>
2026-04-11 14:56:21 +0200

    feat(routes): Add the declarations of the sales route in the main file

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -12,6 +12,7 @@ import (
 	"tili/app/internal/payementmethod"
 	"tili/app/internal/profile"
 	"tili/app/internal/store"
+	"tili/app/internal/sale"
 
 	"tili/app/pkg/db"
 
@@ -70,6 +71,10 @@ func main() {
 	payementmethodService := payementmethod.NewService(payementmethodRepo)
 	payementmethodHandler := payementmethod.NewHandler(payementmethodService)
 
+	saleRepo := sale.NewRepository(db)
+	saleService := sale.NewService(saleRepo)
+	saleHandler := sale.NewHandler(saleService)
+
 	r := gin.Default()
 
 	profileHandler.RegisterRoutes(r)
@@ -80,6 +85,7 @@ func main() {
 	itemHandler.RegisterRoutes(r)
 	categorieHandler.RegisterRoutes(r)
 	payementmethodHandler.RegisterRoutes(r)
+	saleHandler.RegisterRoutes(r)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 

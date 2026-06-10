ce997bfe1aeb65c399012410fed84317a7488c9f
albanemerian <albane.merian@doctolib.com>
2026-06-10 11:26:33 +0200

    feat(catagorie): Udpate the categorie database schema to include a foreign key to a cataloge

--- database/schemas/tables/categories.sql
+++ database/schemas/tables/categories.sql
@@ -1,4 +1,5 @@
 CREATE TABLE categorie (
     categorie_id SERIAL PRIMARY KEY,
-    type VARCHAR(100)
+    type VARCHAR(100),
+    catalog_id INTEGER NOT NULL REFERENCES catalog(catalog_id) ON DELETE CASCADE
 );

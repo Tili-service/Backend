e2f54a8c498d000e918086387562c903ded71b5c
Albane Merian <albane.merian@epitech.eu>
2026-09-21 10:40:50 -0400

    fix(KPI): Add the store uid in the sales tables to avoid cross store data leak

--- database/schemas/tables/06-sales.sql
+++ database/schemas/tables/06-sales.sql
@@ -1,5 +1,6 @@
 CREATE TABLE sales (
     sale_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    store_id UUID NOT NULL REFERENCES store(store_id),
     element JSONB,
     price DECIMAL(10, 2),
     time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
